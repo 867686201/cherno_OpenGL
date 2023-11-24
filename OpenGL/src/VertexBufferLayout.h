e5c9dd9ff34fc8566a0e2bbd493b8ae50033eeb7
867686201 <867686201@qq.com>
2023-11-24 17:18:24 +0800

    14.4 顶点数组类的最终版本

--- OpenGL/src/VertexBufferLayout.h
+++ OpenGL/src/VertexBufferLayout.h
@@ -3,6 +3,7 @@
 #include <vector>
 
 #include "GL/glew.h"
+#include "Renderer.h"
 
 struct VertexLayoutElement
 {
@@ -14,15 +15,25 @@ struct VertexLayoutElement
 	unsigned char normalized; 
 	int GetLength()
 	{
-		int length = 0;
 		switch (type)
 		{
-			case GL_FLOAT: length = 4 * count; break;
-			case GL_UNSIGNED_INT: length = 4 * count; break;
-			case GL_UNSIGNED_BYTE: length = 1 * count; break;
-			default: break;
+			case GL_FLOAT: return 4 * count; 
+			case GL_UNSIGNED_INT: return 4 * count; 
+			case GL_UNSIGNED_BYTE: return 1 * count; 
 		}
-		return length;
+		ASSERT(false);  // 如果没有对应的 type 则终止程序
+		return 0;
+	}
+	static unsigned int GetSizeOfType(unsigned int type) // 视频版本 static 函数不能使用结构体中的变量
+	{
+		switch (type)
+		{
+		case GL_FLOAT: return 4;
+		case GL_UNSIGNED_INT: return 4;
+		case GL_UNSIGNED_BYTE: return 1;
+		}
+		ASSERT(false);
+		return 0;
 	}
 };
 
@@ -34,26 +45,45 @@ public:
 	~VertexBufferLayout() {};
 
 	template<typename T>
-	void Push(int count)
+	void Push(unsigned int count)
 	{
-		static_assert(false);
+		static_assert(false);  // 在编译时, 如果输入了没有在下方特例化的类型, 则会报错
 	}
 	template<>
-	void Push<float>(int count)
+	void Push<float>(unsigned int count)
 	{
-		VertexLayoutElement element{ (unsigned int)count, GL_FLOAT, GL_FALSE };
+		VertexLayoutElement element{ count, GL_FLOAT, GL_FALSE };
 		m_Elements.emplace_back(element);
 		m_Offset.emplace_back(m_Stride);
 		m_Stride += element.GetLength();
+		//m_Stride += count * VertexLayoutElement::GetSizeOfType(GL_FLOAT);
+	}
+	template<>
+	void Push<unsigned int>(unsigned int count)
+	{
+		VertexLayoutElement element{ count, GL_UNSIGNED_INT, GL_FALSE };
+		m_Elements.emplace_back(element);
+		m_Offset.emplace_back(m_Stride);
+		m_Stride += element.GetLength();
+		//m_Stride += count * VertexLayoutElement::GetSizeOfType(GL_UNSIGNED_INT);
+	}
+	template<>
+	void Push<unsigned char>(unsigned int count)
+	{
+		VertexLayoutElement element{ count, GL_UNSIGNED_BYTE, GL_TRUE };
+		m_Elements.emplace_back(element);
+		m_Offset.emplace_back(m_Stride);
+		m_Stride += element.GetLength();
+		//m_Stride += count * VertexLayoutElement::GetSizeOfType(GL_UNSIGNED_BYTE);
 	}
 
 	inline VertexLayoutElement GetElement(unsigned int index) const { return m_Elements[index]; }
-	inline std::vector<VertexLayoutElement> GetElements() const { return m_Elements; }
-	inline int GetStride() const { return m_Stride; }
-	inline int GetOffset(int i) const { return m_Offset[i]; }
+	inline const std::vector<VertexLayoutElement> GetElements() const { return m_Elements; } // m_Elements 返回后不想要被修改, 在前方加入 const
+	inline unsigned int GetStride() const { return m_Stride; }
+	inline unsigned int GetOffset(int i) const { return m_Offset[i]; }
 
 private:
 	std::vector<VertexLayoutElement> m_Elements;
 	std::vector<unsigned int> m_Offset;
-	int m_Stride;
+	unsigned int m_Stride;
 };
