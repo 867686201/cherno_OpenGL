c44e52d2d0dee01b52135888b57047fe2c08973b
867686201 <867686201@qq.com>
2023-11-24 15:52:20 +0800

    14.3 补充，修改后可以删除成员变量 m_Count

--- OpenGL/src/VertexBufferLayout.h
+++ OpenGL/src/VertexBufferLayout.h
@@ -45,18 +45,15 @@ public:
 		m_Elements.emplace_back(element);
 		m_Offset.emplace_back(m_Stride);
 		m_Stride += element.GetLength();
-		m_Count++;
 	}
 
 	inline VertexLayoutElement GetElement(unsigned int index) const { return m_Elements[index]; }
 	inline std::vector<VertexLayoutElement> GetElements() const { return m_Elements; }
-	inline unsigned int GetCount() const { return m_Count; }
 	inline int GetStride() const { return m_Stride; }
 	inline int GetOffset(int i) const { return m_Offset[i]; }
 
 private:
 	std::vector<VertexLayoutElement> m_Elements;
-	unsigned int m_Count;  // Elements µÄ¸öÊı
 	std::vector<unsigned int> m_Offset;
 	int m_Stride;
 };
